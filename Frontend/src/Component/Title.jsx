bd565b589d4a92e9142c4a22b5f2a625d7c12988
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-05-17 22:06:26 +0600

    title has addes in only HTML formate

---
+++ Frontend/src/Component/Title.jsx
@@ -0,0 +1,8 @@
+<div class="m-auto">
+  <h3 class="text-center m-auto text-3xl text-[#0e1133] font-bold">
+    Your Title Here
+  </h3>
+  <p class="max-w-[90%] font1 text-center m-auto md:max-w-[60%]">
+    Your subtitle or description goes here. Make sure it's concise and clear.
+  </p>
+</div>;
