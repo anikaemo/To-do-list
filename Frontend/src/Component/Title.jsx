db502ea72e99a994753892abc8c2e09f2495acbf
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-05-17 22:08:56 +0600

    Title converted into React

--- Frontend/src/Component/Title.jsx
+++ Frontend/src/Component/Title.jsx
@@ -1,8 +1,14 @@
-<div class="m-auto">
-  <h3 class="text-center m-auto text-3xl text-[#0e1133] font-bold">
-    Your Title Here
-  </h3>
-  <p class="max-w-[90%] font1 text-center m-auto md:max-w-[60%]">
-    Your subtitle or description goes here. Make sure it's concise and clear.
-  </p>
-</div>;
+const Title = ({ title, subtitle }) => {
+  return (
+    <div className="m-auto">
+      <h3 className="text-center m-auto text-3xl text-[#0e1133] font-bold">
+        {title}
+      </h3>
+      <p className="max-w-[90%] font1 text-center m-auto md:max-w-[60%]">
+        {subtitle}
+      </p>
+    </div>
+  );
+};
+
+export default Title;
