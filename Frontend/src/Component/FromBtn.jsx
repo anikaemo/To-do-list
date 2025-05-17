f34c4ac9be778cd341391abb71a577bf0e44e5fb
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-05-17 21:49:46 +0600

    Form Button addes in HTML

---
+++ Frontend/src/Component/FromBtn.jsx
@@ -0,0 +1,6 @@
+<button
+  type="submit"
+  class="text-[#801f82] border-2 w-full border-[#801f82] px-4 py-2 md:px-5 md:py-2 font-semibold text-sm md:text-base rounded-md hover:bg-[#801f82] hover:text-white"
+>
+  Submit
+</button>;
