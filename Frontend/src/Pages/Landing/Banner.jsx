d780c39f570733f22b2a4115440f49007ce140ef
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-05-18 20:03:56 +0600

    Banner Updated

--- Frontend/src/Pages/Landing/Banner.jsx
+++ Frontend/src/Pages/Landing/Banner.jsx
@@ -1,35 +1,35 @@
-<div class="bg-[#f9f9f9]">
-  <div class="hero md:min-h-[500px] relative">
-    <div class="hero-content flex-col-reverse md:flex-row-reverse">
-      
-      <!-- Banner Image -->
-      <div class="md:w-1/2">
-        <img src="/path/to/hero.png" class="w-10/12 mx-auto" alt="Hero" />
-      </div>
-
-      <!-- Banner Text -->
-      <div class="md:w-1/2">
-        <h1 class="text-5xl lg:text-7xl text-[#0e1133] font-bold">
-          FREE ONLINE TASK MANAGER
-        </h1>
-        <p class="py-6 md:max-w-[80%] font1 font-medium">
-          Organize and manage your team like a boss with Bitrix24, a free task
-          management tool packing more capabilities than you can imagine.
-        </p>
-
-        <!-- Button -->
-        <div class="flex gap-5">
-          <a href="/signin">
-            <button
-              type="button"
-              class="text-[#801f82] border-2 border-[#801f82] px-5 py-2 font-semibold text-base rounded-md hover:bg-[#801f82] hover:text-white"
-            >
-              Let’s Explore
-            </button>
-          </a>
+import { useContext } from "react";
+import Btn from "../../Component/Btn";
+import bannerImg from "/src/assets/hero.png";
+import { AuthContext } from "../../Authantication/AuthProvider/AuthProvider";
+const Banner = () => {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="bg-[#f9f9f9]">
+      <div className="hero md:min-h-[500px] relative">
+        <div className="hero-content flex-col-reverse md:flex-row-reverse">
+          <div className="md:w-1/2">
+            <img src={bannerImg} className=" w-10/12 m-auto" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className=" text-5xl lg:text-7xl text-[#0e1133] font-bold">
+              FREE ONLINE TASK MANAGER
+            </h1>
+            <p className="py-6 md:max-w-[80%] font1 font-medium">
+              Organize and manage your team like a boss with Bitrix24, a free
+              task management tool packing more capabilities than you can
+              imagine.
+            </p>
+            <div className=" flex gap-5">
+              <a href={user ? "/users" : "/signin"}>
+                <Btn btnName={"Let’s Explore"}></Btn>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  </div>
-</div>
+  );
+};
+
+export default Banner;
