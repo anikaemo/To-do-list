393810dcc809191c861a2db10009e5368333658a
MESHRAF <meshrafahmed612@gmail.com>
2025-05-18 19:42:46 +0600

    Banner add in HTML

---
+++ Frontend/src/Pages/Landing/Banner.jsx
@@ -0,0 +1,35 @@
+<div class="bg-[#f9f9f9]">
+  <div class="hero md:min-h-[500px] relative">
+    <div class="hero-content flex-col-reverse md:flex-row-reverse">
+      
+      <!-- Banner Image -->
+      <div class="md:w-1/2">
+        <img src="/path/to/hero.png" class="w-10/12 mx-auto" alt="Hero" />
+      </div>
+
+      <!-- Banner Text -->
+      <div class="md:w-1/2">
+        <h1 class="text-5xl lg:text-7xl text-[#0e1133] font-bold">
+          FREE ONLINE TASK MANAGER
+        </h1>
+        <p class="py-6 md:max-w-[80%] font1 font-medium">
+          Organize and manage your team like a boss with Bitrix24, a free task
+          management tool packing more capabilities than you can imagine.
+        </p>
+
+        <!-- Button -->
+        <div class="flex gap-5">
+          <a href="/signin">
+            <button
+              type="button"
+              class="text-[#801f82] border-2 border-[#801f82] px-5 py-2 font-semibold text-base rounded-md hover:bg-[#801f82] hover:text-white"
+            >
+              Let’s Explore
+            </button>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
